386ab59a727d1b0e4bafd43ffa3af7e9d366a937
NHThang <thangnh2812@gmail.com>
2026-04-22 21:33:53 +0700

    Folder structure

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+
+int main(){
+
+
+    int choice;
+    while(true){
+        std::cout << "Welcome to the Mini Baking System!" << std::endl;
+        std::cout << "Please select an option:" << std::endl;
+        std::cout << "1. Create account" << std::endl;
+        std::cout << "2. Login" << std::endl;
+        std::cout << "3. Exit" << std::endl;
+        std::cin >> choice;
+
+        // switch (choice)
+        // {
+        // case 1: bank.CreateAccount();
+        //     break;
+        // case 2: bank.Login();
+        //     break;
+        // case 3: 
+        //     bank.saveData();
+        //     std::cout << "Thank you for using the Mini Baking System. Goodbye!" << std::endl;
+        //     return 0;
+        // default:
+        //     std::cout << "Invalid option. Please try again." << std::endl;
+        // }
+
+    }
+}
